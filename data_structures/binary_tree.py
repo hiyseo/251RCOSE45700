fc6576ada4c10db99ffac3eefabc83c1f759ac11
hiyseo <seo3167@naver.com>
2025-06-15 16:29:17 +0900

    add: binary_tree

---
+++ data_structures/binary_tree.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if key < node.key:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert(node.left, key)
+        else:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert(node.right, key)
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            print(node.key, end=" ")
+            self.inorder(node.right)
+
+if __name__ == "__main__":
+    bst = BinarySearchTree()
+    for num in [7, 3, 9, 1, 5, 8, 10]:
+        bst.insert(num)
+
+    print("중위 순회 결과:")
+    bst.inorder(bst.root)
+    print()
